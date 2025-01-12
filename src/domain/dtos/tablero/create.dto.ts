6d35eba945ccdc0cb23a49816f3487b247d6a0ac
Walter Pérez <jose.perezt@salud.gob.sv>
2025-01-12 17:52:08 -0600

    fix: corrigiendo bug

--- src/domain/dtos/tablero/create.dto.ts
+++ src/domain/dtos/tablero/create.dto.ts
@@ -1,19 +1,26 @@
 import {CustomError} from '../../errors/CustomError';
 
 export interface CreateTableroDtoProps {
-    nombre: string;
-    descripcion: string;
+	nombre: string;
+	descripcion: string;
 }
 export class CreateTableroDto {
-    private constructor(public readonly nombre: string, public readonly descripcion: string) {}
-    static create(props: CreateTableroDtoProps) {
-        const {nombre, descripcion} = props;
-        if (nombre.trim() === '' || nombre == null) {
-            CustomError.UnprocessableEntity('Nombre es obligatorio');
-        }
-        if (descripcion.trim() === '' || descripcion == null) {
-            CustomError.UnprocessableEntity('Descripción es obligatorio');
-        }
-        return new CreateTableroDto(nombre, descripcion);
-    }
+	private constructor(public readonly nombre: string, public readonly descripcion: string) {}
+	static create(props: CreateTableroDtoProps) {
+		const {nombre, descripcion} = props;
+        console.log("nombre", nombre == null)
+		if (nombre == null) {
+			throw CustomError.UnprocessableEntity('Nombre es obligatorio');
+		}
+		if (descripcion == null) {
+			throw CustomError.UnprocessableEntity('Descripción es obligatorio');
+		}
+		if (nombre?.trim() === '' || nombre == null) {
+			throw CustomError.UnprocessableEntity('Nombre es obligatorio');
+		}
+		if (descripcion?.trim() === '' || descripcion == null) {
+			throw CustomError.UnprocessableEntity('Descripción es obligatorio');
+		}
+		return new CreateTableroDto(nombre, descripcion);
+	}
 }
